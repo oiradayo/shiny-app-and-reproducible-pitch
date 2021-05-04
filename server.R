3fc01410b489f78e9092491678cdefa768b9c801
oiradayo <76747755+oiradayo@users.noreply.github.com>
2021-05-05 08:53:33 +0900

    Create server.R

---
+++ server.R
@@ -0,0 +1,8 @@
+shinyServer(function(input, output) {
+
+  output$textOut01 <- renderText({         
+    numInput <- as.numeric(input$textIn01)    
+    result <- numInput * 2           
+  })
+
+})
